35bb7c3e115595fe2cdc81e40aab12e500d50585
Roanna Victorio <roannav@users.noreply.github.com>
2024-03-21 15:12:59 -0700

    rm unneeded Vite boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import StockTable from "./components/StockTable";
 import StockChart from "./components/StockChart";
 import LineChart from "./components/LineChart";
 import CandleStickChart from "./components/CandleStickChart";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -13,29 +11,16 @@ function App() {
 
   return (
     <>
+      <h1>Stocks</h1>
+      <div className="card">
+        <Button type="primary" onClick={()=> {console.log("Clicked button 1")}}>Button 1</Button>
+      </div>
       <div>
-        <StockChart symbol={'GOOG'} />
+        {/* <StockChart symbol={'GOOG'} /> */}
         <CandleStickChart />
         <LineChart />
         {/* <StockTable /> */}
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button type="primary" onClick={()=> {console.log("Clicked button 1")}}>Button 1</Button>
     </>
   );
 }
