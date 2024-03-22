5d2b44b114e0b5706daf6a8eb3dbef15819f7c1a
Roanna Victorio <roannav@users.noreply.github.com>
2024-03-22 00:07:33 -0700

    Add an autocomplete to select which stock symbol.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import type { RadioChangeEvent } from "antd";
-import { Radio } from "antd";
-import { Button } from "antd";
+import { Radio, Button, Space, AutoComplete } from "antd";
 import StockTable from "./components/StockTable";
 import StockChart from "./components/StockChart";
 import LineChart from "./components/LineChart";
 import CandleStickChart from "./components/CandleStickChart";
 import "./App.css";
 
+const options = [
+  { value: "AAPL" },
+  { value: "AMZN" },
+  { value: "GOOG" },
+  { value: "META" },
+  { value: "MSFT" },
+  { value: "NVDA" },
+  { value: "RDDT" },
+];
+
 function App() {
   const [timeGranularity, setTimeGranularity] = useState("Daily");
+  const [symbol, setSymbol] = useState("");
 
-  const onChange = (e: RadioChangeEvent) => {
+  const onTimeGranularityChange = (e: RadioChangeEvent) => {
     console.log(`radio checked:${e.target.value}`);
     setTimeGranularity(e.target.value);
   };
@@ -30,15 +40,38 @@ function App() {
         </Button>
       </div>
       <div className="card">
-        Choose a Time Granularity:
-        <Radio.Group onChange={onChange} defaultValue="Daily">
-          <Radio.Button value="Daily">Daily</Radio.Button>
-          <Radio.Button value="Weekly">Weekly</Radio.Button>
-          <Radio.Button value="Monthly">Monthly</Radio.Button>
-        </Radio.Group>
+        <Space>
+          Stock Symbol:
+          <AutoComplete
+            style={{ width: 200 }}
+            options={options}
+            placeholder="try to type `b`"
+            filterOption={(inputValue, option) =>
+              option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+              -1
+            }
+            allowClear={true}
+            onChange={()=>{console.log("AutoComplete onChange")}}
+            onSelect={(value: string )=>{
+              console.log("AutoComplete onSelect");
+              console.log(value);
+              setSymbol(value);
+            }}
+          />
+        </Space>
+      </div>
+      <div className="card">
+        <Space>
+          Time Granularity:
+          <Radio.Group onChange={onTimeGranularityChange} defaultValue="Daily">
+            <Radio.Button value="Daily">Daily</Radio.Button>
+            <Radio.Button value="Weekly">Weekly</Radio.Button>
+            <Radio.Button value="Monthly">Monthly</Radio.Button>
+          </Radio.Group>
+        </Space>
       </div>
       <div>
-        <StockChart symbol={'MSFT'} timeGranularity={timeGranularity} />
+        <StockChart symbol={symbol} timeGranularity={timeGranularity} />
         <CandleStickChart />
         <LineChart />
         {/* <StockTable /> */}
